5fdddf25a86b0d5756f8adfb4c5dd16a275ed57b
rihemabassi12345 <rihemabassi30@gmail.com>
2026-09-16 15:08:00 +0200

    Update print statement to greet with 'hiii'

--- main.py
+++ main.py
@@ -60,4 +60,4 @@ modele_tflite = convertisseur.convert()
 
 with open(CHEMIN_MODELE_TFLITE, "wb") as f:
     f.write(modele_tflite)
-print(f"✅ hello{CHEMIN_MODELE_TFLITE}")
+print(f"✅ hiii{CHEMIN_MODELE_TFLITE}")
